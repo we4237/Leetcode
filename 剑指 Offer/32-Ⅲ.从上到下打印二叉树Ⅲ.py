d9cf1129b07f5224b2d72f0ed2f471d67873c931
534424899@qq.com <534424899@qq.com>
2023-03-17 10:43:34 +0800

    Leetcode学习笔记

--- 剑指 Offer/32-Ⅲ.从上到下打印二叉树Ⅲ.py
+++ 剑指 Offer/32-Ⅲ.从上到下打印二叉树Ⅲ.py
@@ -11,33 +11,51 @@ class TreeNode:
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[int]:
-        if root is None:return []
+        # if root is None:return []
+        # queue = collections.deque()
+        # queue.append(root)
+        # ans = []
+        # while queue:
+        #     tmp = []
+        #
+        #     for _ in range(len(queue)):
+        #
+        #         node = queue.popleft()
+        #         tmp.append(node.val)
+        #         if node.left :queue.append(node.left)
+        #         if node.right:queue.append(node.right)
+        #     ans.append(tmp)
+        #
+        #     if not queue :
+        #         break
+        #
+        #     tmp = []
+        #
+        #     for _ in range(len(queue)):
+        #
+        #         node = queue.pop()
+        #         tmp.append(node.val)
+        #         if node.right:queue.appendleft(node.right)
+        #         if node.left :queue.appendleft(node.left)
+        #
+        #     ans.append(tmp)
+        #
+        # return ans
+
+        if not root: return []
         queue = collections.deque()
         queue.append(root)
-        ans = []
+        res = []
         while queue:
-            tmp = []
-
+            tmp = collections.deque()
             for _ in range(len(queue)):
-
                 node = queue.popleft()
-                tmp.append(node.val)
-                if node.left :queue.append(node.left)
-                if node.right:queue.append(node.right)
-            ans.append(tmp)
+                if len(res) % 2 == 0:
+                    tmp.appendleft(node.val)
+                else:
+                    tmp.append(node.val)
+                if node.right: queue.append(node.right)
+                if node.left: queue.append(node.left)
+            res.append(list(tmp))
 
-            if not queue :
-                break
-
-            tmp = []
-
-            for _ in range(len(queue)):
-
-                node = queue.pop()
-                tmp.append(node.val)
-                if node.right:queue.appendleft(node.right)
-                if node.left :queue.appendleft(node.left)
-
-            ans.append(tmp)
-
-        return ans
+        return res
